5a4f06a8d0aaf918439f622c9a98677431366cae
Nguyen Hoang Duc <duc.nguyen@minhlongi.com>
2023-10-31 15:37:03 +0700

    add example

---
+++ example/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { CardProduct } from 'react-component-style'
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <React.StrictMode>
+    <div>
+      <h2>Default counter</h2>
+      <CardProduct name='123412' />
+    </div>
+  </React.StrictMode>,
+)
