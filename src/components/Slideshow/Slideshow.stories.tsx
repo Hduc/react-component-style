2c1042de3b4e0634a3cbc911172d0fb7b3fbdec8
Nguyen Hoang Duc <38237006+Hduc@users.noreply.github.com>
2024-01-24 17:38:46 +0700

    Update Slideshow.stories.tsx

--- src/components/Slideshow/Slideshow.stories.tsx
+++ src/components/Slideshow/Slideshow.stories.tsx
@@ -3,23 +3,42 @@ import { Meta, StoryObj } from "@storybook/react";
 import Slideshow from "./Slideshow";
 
 const meta: Meta<typeof Slideshow> = {
-    component: Slideshow,
-    title: "Image/Slideshow",
-    argTypes: {},
+  component: Slideshow,
+  title: "Image/Slideshow",
+  argTypes: {},
 };
 export default meta;
 type Story = StoryObj<typeof Slideshow>;
 
 export const Default: Story = (args) => (
-  <div style={{width:'70%',margin:'5px auto'}}>
+  <div style={{ width: '60%', margin: '5px auto' }}>
     <Slideshow data-test-id="Slideshow-id" {...args} />
   </div>
 );
 Default.args = {
-  active:0,
-    data:[
-    {id:'1234',url:'https://product.minhlong.com/product/01071100903/avatar.png',name:'01071100903'},
-    {id:'1235',url:'https://product.minhlong.com/product/010711139/avatar.png',name:'010711139'},
-    {id:'1236',url:'https://product.minhlong.com/product/01071113903/avatar.png?v=1',name:'01071113903'},
-    ]
+  active: 0,
+  showThumb:true,
+  showAction:true,
+  automatic:2000,
+  data: [
+    { id: '1234', url: 'https://product.minhlong.com/product/01071100903/avatar.png', name: '01071100903' },
+    { id: '1235', url: 'https://product.minhlong.com/product/010711139/avatar.png', name: '010711139' },
+    { id: '1236', url: 'https://product.minhlong.com/product/01071113903/avatar.png?v=1', name: '01071113903' },
+  ]
+};
+export const FullWidth: Story = (args) => (
+  <div style={{ width: '100%' }}>
+    <Slideshow data-test-id="Slideshow-id" {...args} />
+  </div>
+);
+FullWidth.args = {
+  active: 0,
+  showThumb:false,
+  showAction:false,
+  automatic:2000,
+  data: [
+    { id: '1234', url: 'https://product.minhlong.com/product/01071100903/avatar.png', name: '01071100903' },
+    { id: '1235', url: 'https://product.minhlong.com/product/010711139/avatar.png', name: '010711139' },
+    { id: '1236', url: 'https://product.minhlong.com/product/01071113903/avatar.png?v=1', name: '01071113903' },
+  ]
 };
